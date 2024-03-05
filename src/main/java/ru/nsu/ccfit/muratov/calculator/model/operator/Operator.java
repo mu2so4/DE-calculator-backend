9d9624576f6b07680067bd7f65d79103ddc1fcad
mu2so4 <b4.orangutang@gmail.com>
2024-03-05 21:22:48 +0700

    Move some implementation into interface

--- src/main/java/ru/nsu/ccfit/muratov/calculator/model/operator/Operator.java
+++ src/main/java/ru/nsu/ccfit/muratov/calculator/model/operator/Operator.java
@@ -2,8 +2,18 @@ package ru.nsu.ccfit.muratov.calculator.model.operator;
 
 import ru.nsu.ccfit.muratov.calculator.model.Context;
 
-public interface Operator {
-    double evaluate(double... values);
+public abstract class Operator {
+    public final double evaluate(double[] values) {
+        //check if we got proper count of operands for some action
+        if(values.length != getOperandCount()) {
+            throw new IllegalArgumentException(
+                String.format("expected %d operands, got %d", getOperandCount(), values.length)
+            );
+        }
+        return performEvaluation(values);
+    }
 
-    int getOperandCount();
+    protected abstract double performEvaluation(double[] values);
+
+    public abstract int getOperandCount();
 }
--- src/main/java/ru/nsu/ccfit/muratov/calculator/model/operator/Plus.java
+++ src/main/java/ru/nsu/ccfit/muratov/calculator/model/operator/Plus.java
@@ -1,20 +1,13 @@
 package ru.nsu.ccfit.muratov.calculator.model.operator;
 
-public class Plus implements Operator {
-    private static final int OPERAND_COUNT = 2;
+public class Plus extends Operator {
     @Override
-    public double evaluate(double... values) {
-        //check if we got proper count of operands for addition
-        if(values.length != OPERAND_COUNT) {
-            throw new IllegalArgumentException(
-                String.format("expected %d operands, got %d", OPERAND_COUNT, values.length)
-            );
-        }
+    protected double performEvaluation(double[] values) {
         return values[0] + values[1];
     }
 
     @Override
     public int getOperandCount() {
-        return OPERAND_COUNT;
+        return 2;
     }
 }
